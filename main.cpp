12c0384ac26a184247780b1239361add1ababb6f
Marek Grencštein <marekgrencstein@gmail.com>
2016-12-21 20:12:09 +0200

    Removed unrelated GUI tools

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "Map/MapModule.h"
 #include "Move/MoveModule.h"
 #include "Radar/RadarModule.h"
-#include "GuiTools/GuiModule.h"
 
 int main(int argc, char ** argv)
 {
@@ -9,19 +8,13 @@ int main(int argc, char ** argv)
     RadarModule radar;
     MoveModule move;
     MapModule map;
-    GuiModule gui;
 
     map.SetRadarModule(&radar);
     map.SetMoveModule(&move);
-    gui.SetRadarModule(&radar);
-    gui.SetMapModule(&map);
 
     radar.Execute();
     map.Execute();
     move.Execute();
-    gui.Execute();
-
-    while (gui.WindowNotClosed());
 
     return 0;
 }
